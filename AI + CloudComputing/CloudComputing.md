9ee934cf2d3fcd8da2dc32e736af34fdb84241fb
yu.xi <yu.xi@laserfiche.com>
2024-05-11 15:53:37 +0800

    added galaxy classification md

---
+++ AI + CloudComputing/CloudComputing.md
@@ -0,0 +1,4 @@
+https://www.sap.cn/products/technology-platform/what-is-cloud-computing.html
+
+https://azure.microsoft.com/zh-cn/resources/cloud-computing-dictionary/what-is-paas/
+
---
+++ AI + CloudComputing/HandsOn/GalaxyClassification.py
@@ -0,0 +1,30 @@
+# Galaxy Multi-Image Classification with LeNet-5
+# https://www.kaggle.com/code/tenzinmigmar/galaxy-multi-image-classification-with-lenet-5/notebook
+
+# load with astroNN: astroNN is a python package to do various kinds of neural networks with targeted application in astronomy by using Keras API as model and training prototyping, but at the same time take advantage of Tensorflow’s flexibility.
+# https://astronn.readthedocs.io/en/latest/galaxy10.html
+
+# Importing Dependencies
+
+import tensorflow as tf
+
+'''import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from tensorflow import keras
+
+from keras.models import Sequential
+from keras.layers import Conv2D, AveragePooling2D, Flatten, Dense, Dropout
+from keras.callbacks import ReduceLROnPlateau
+from keras.optimizers import Adam
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+from tensorflow.keras import utils
+
+from astroNN.datasets import galaxy10
+from astroNN.datasets.galaxy10 import galaxy10cls_lookup'''
+
